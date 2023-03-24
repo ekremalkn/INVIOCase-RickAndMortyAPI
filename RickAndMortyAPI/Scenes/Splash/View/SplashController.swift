e71c69b2cd4a1343504013ce1cfebee557812995
Ekrem Alkan <ae.alkanekrem@gmail.com>
2023-03-24 12:47:37 +0300

    MVVM file structure has been set up and third-party libraries have been added.

---
+++ RickAndMortyAPI/Scenes/Splash/View/SplashController.swift
@@ -0,0 +1,25 @@
+//
+//  SplashController.swift
+//  RickAndMortyAPI
+//
+//  Created by Ekrem Alkan on 24.03.2023.
+//
+
+import UIKit
+
+final class SplashController: UIViewController {
+    
+    //MARK: - Life Cycle Methods
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .yellow
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
